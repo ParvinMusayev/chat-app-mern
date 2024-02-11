1d8de4e6482999b0a46c42ce3f5341263cd0a2f4
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-11 17:56:08 +0400

    feat: Add mongo db connect

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,18 @@
-export const signup = (req, res) => {
-  console.log("signupUser");
+export const signup = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export const login = (req, res) => {
+  res.send("loginUser");
   console.log("loginUser");
 };
 
 export const logout = (req, res) => {
+  res.send("logoutUser");
   console.log("logoutUser");
 };
