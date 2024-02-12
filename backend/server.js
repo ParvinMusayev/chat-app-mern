f2be84ae0819a7798369d85f80977da3584c83c2
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-12 16:24:18 +0400

    feat: Add login function

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,6 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-// app.get("/", (req, res) => {
-//   res.send("HEllo world!");
-// });
-
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 
 app.use("/api/auth", authRoutes);
