5b035d933d462d74fb2fd0e40bcb08386c66329f
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-15 15:43:13 +0400

    chore: update message controller

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,6 +18,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
