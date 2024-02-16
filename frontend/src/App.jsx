6a40df5d105a1cfe0b6a87f4994343017ff0eab4
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-16 06:01:40 +0400

    chore: update pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
-import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+//pages
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
   );
 }
