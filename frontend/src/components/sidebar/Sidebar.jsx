92f87e5b1929e7cf21fe1494c0cbe937cff592d2
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-16 04:03:43 +0400

    feat: Add Sidebar Components

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Conversations from "./Conversations";
+import LogoutButton from "./LogoutButton";
+import SearchInput from "./SearchInput";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
-
-export default Sidebar
+    <div className="border-r border-slate-500 p-4 flex flex-col">
+      <SearchInput />
+      <div className="divider px-3"></div>
+      <Conversations />
+      <LogoutButton />
+    </div>
+  );
+};
+export default Sidebar;
