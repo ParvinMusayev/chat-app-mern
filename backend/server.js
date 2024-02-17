e330b819be6a360cf6bedc10a8bd19e34506bd99
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-17 22:08:17 +0400

    feat: Add Socket context

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
-const app = express();
+import { app, server } from "./socket/socket.js";
+
+
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -20,7 +22,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server running on port ${PORT}`);
 });
