96d915824d73b69cf37252d9e7532b0e812b9ead
ParvinMusayev <parvinmusayev1@gmail.com>
2024-02-17 17:46:08 +0400

    feat: Add emojis util

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,14 +1,13 @@
+import useGetConversations from "../../hooks/useGetConversations";
 import Conversation from "./Conversation";
 
 const Conversations = () => {
+	const { loading, conversations } = useGetConversations();
 	return (
 		<div className='py-2 flex flex-col overflow-auto'>
-			<Conversation />
-			<Conversation />
-			<Conversation />
-			<Conversation />
-			<Conversation />
-			<Conversation />
+		
+
+		{loading ? <span className='loading loading-spinner mx-auto'></span> : null}
 		</div>
 	);
 };
